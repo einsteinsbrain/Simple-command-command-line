279787e8ecb237ba642aba2273263591dc0946b4
einsteinsbrain <115792403+einsteinsbrain@users.noreply.github.com>
2022-10-14 11:24:52 +0000

    Add files via upload

---
+++ program.py
@@ -0,0 +1,131 @@
+line0 = "          \n"
+line1 = "          \n"
+line2 = "          \n"
+line3 = "          \n"
+line4 = "          \n"
+line5 = "          \n"
+line6 = "          \n"
+line7 = "          \n"
+line8 = "          \n"
+line9 = "          \n"
+#print(line9)
+#print(len(line9))
+x = -1 #starts at -1
+y = 0 #starts at 0
+Run  = 0
+while Run == 0:
+    screen = [line0,line1,line2,line3,line4,line5,line6,line7,line8,line9]
+    screen[y] = screen[y][:x+1:] + "X" + screen[y][x+2::]
+    screenOutput = str(screen).replace(",", "")
+    screenOutput = screenOutput.replace("[", "")
+    screenOutput = screenOutput.replace("]", "")
+    screenOutput = screenOutput.replace("\'", "")
+    screenOutput = "('" + screenOutput + "')"
+    screenOutput = 'print' + screenOutput 
+    exec(screenOutput)
+    #print(line9[::])
+    #print(len(line9[::]))
+    move = input("please move \"X\":")
+
+    if move[:1:] == "N":
+        Nmove = [move]
+        Nmove = Nmove.copy()
+        Nmove = str(Nmove)
+        Nmove = Nmove.replace("[", "")
+        Nmove = Nmove.replace("]", "")
+        Nmove = Nmove.replace("'", "")
+        move = Nmove
+        NRun = True
+        while NRun == True:
+            if move[1:2:] == "1":
+                if y+1 > 9:
+                    print("Out of Bounds\!")
+                    NRun = False
+                    Ny = Nmove.replace("N", "")
+                    y = Ny
+                else:
+                    y+=1
+            elif move[1:2:] == "2":
+                if y+2 > 9:
+                    print("Out of Bounds!")
+                    NRun = False
+                    Ny = Nmove.replace("N", "")
+                    y = Ny
+                else: y+=2
+    elif move[:1:] == "S":
+        Smove = [move]
+        Smove = Smove.copy()
+        Smove = str(Smove)
+        Smove = Smove.replace("[", "")
+        Smove = Smove.replace("]", "")
+        Smove = Smove.replace("'", "")
+        move = Smove
+        while SRun == True:
+            if move[1:2:] == "1":
+                if y-1 < 0:
+                    print("Out of Bounds!")
+                    SRun = False
+                    Sy = Smove.replace("S", "")
+                    y = Sy
+                else:
+                    y-=1
+            elif move[1:2:] == "2":
+                if y-2 > 0:
+                    print("Out of Bounds!")
+                    SRun = False
+                    Sy = Smove.replace("S", "")
+                    y = Sy
+                else: y-=2
+    elif move[:1:] == "W":
+        Wmove = [move]
+        Wmove = Wmove.copy()
+        Wmove = str(Wmove)
+        Wmove = Wmove.replace("[", "")
+        Wmove = Wmove.replace("]", "")
+        Wmove = Wmove.replace("'", "")
+        move = Wmove
+        WRun = True
+        while WRun == True:
+            if move[1:2:] == "1":
+                if x-1 < -1:
+                    print("Out of Bounds!")
+                    WRun = False
+                    Wy = Wmove.replace("W", "")
+                    y = Wy
+                else:
+                        x-=1
+            elif move[1:2:] == "2":
+                if x-2 > -1:
+                    print("Out of Bounds!")
+                    WRun = False
+                    Wy = Wmove.replace("W", "")
+                    y = Wy
+                else: x-=2
+    elif move[:1:] == "E":
+        Wmove = [move]
+        Wmove = Emove.copy()
+        Wmove = str(Emove)
+        Wmove = Emove.replace("[", "")
+        Emove = Emove.replace("]", "")
+        Emove = Emove.replace("'", "")
+        move = Emove
+        ERun = True
+        while ERun == True:
+            if move[1:2:] == "1":
+                if x+1 > 8:
+                    print("Out of Bounds!")
+                    Ey = Emove.replace("E", "")
+                    y = Ey
+                    ERun = False
+                else:
+                    x+=1
+            elif move[1:2:] == "2":
+                if x+2 > 8:
+                    print("Out of Bounds!")
+                    Ey = Emove.replace("E", "")
+                    y = Ey
+                    ERun = False
+                else: x+=2
+    else:
+        print("You seem to have entered an Invalid Move!")
+        Run = 1
